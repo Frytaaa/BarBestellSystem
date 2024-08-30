91d2eba6f2724eb8ab71933ff6e5c66459a27fca
FrydrycM <frydrycm@apetito.com>
2024-08-30 14:09:14 +0200

    db

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -7,9 +7,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        var dbPath = Path.Join(path, "bar.db");
+        var exePath = AppDomain.CurrentDomain.BaseDirectory;
+        var solutionPath = Directory.GetParent(exePath).Parent.Parent.Parent.Parent.FullName;
+
+        var dbPath = Path.Join(solutionPath, "bar.db");
         services.AddDbContext<BarDbContext>(options =>
         {
             options.UseSqlite($"Data Source={dbPath}");
